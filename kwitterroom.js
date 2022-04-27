7218267ee7938aad8889733a27b15780b2c9c397
codingprojectsandcoding <95212367+codingprojectsandcoding@users.noreply.github.com>
2022-04-27 15:17:43 +0530

    Add files via upload

---
+++ kwitterroom.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCM168s6eeHI_vyvq9HgJowx8nf7FulsOE",
+    authDomain: "project-91-4a8f6.firebaseapp.com",
+    databaseURL: "https://project-91-4a8f6-default-rtdb.firebaseio.com",
+    projectId: "project-91-4a8f6",
+    storageBucket: "project-91-4a8f6.appspot.com",
+    messagingSenderId: "269703507184",
+    appId: "1:269703507184:web:b9b76926c83d76a5a8e635",
+    measurementId: "G-3Z8487E74W"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
